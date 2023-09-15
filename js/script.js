c7d51504d6f21a1c8854ae5a95e339b5cfcbd14d
Developer <112705739+Farrux73982011@users.noreply.github.com>
2023-09-15 18:22:38 +0500

    site ready

--- js/script.js
+++ js/script.js
@@ -14,60 +14,49 @@ reload(arr)
 
 function reload(data) {	
 	container_box.innerHTML = ''
-	
+
 	for (let item of data) {
 		let divBoxItem = document.createElement('div');
-		divBoxItem.classList.add('box-item');
-
 		let divItemTop = document.createElement('div');
-		divItemTop.classList.add('item-top');
-
 		let imgBag = document.createElement('img');
-		imgBag.src = item.image;
-
 		let divItemBottom = document.createElement('div');
-		divItemBottom.classList.add('item-bottom');
-
 		let h2SupTitle = document.createElement('h2');
+		let description = document.createElement('p');
+		let divSubTitle = document.createElement('div');
+		let divTitlePrice = document.createElement('div');
+		let imgPrice = document.createElement('img');
+		let spanPrice = document.createElement('span');
+		let divTitleRating = document.createElement('div');
+		let imgRating = document.createElement('img');
+		let spanRating = document.createElement('span');
+		let divTitleCount = document.createElement('div');
+		let imgCount = document.createElement('img');
+		let spanCount = document.createElement('span');
+		let buttonFavorite = document.createElement('button');
+
+
+
+		divBoxItem.classList.add('box-item');
+		divItemTop.classList.add('item-top');
+		imgBag.src = item.image;
+		divItemBottom.classList.add('item-bottom');
 		h2SupTitle.classList.add('sup-title');
 		h2SupTitle.textContent = item.title;
-
-		let description = document.createElement('p');
 		description.classList.add('description');
 		description.innerHTML = item.description
-
-		let divSubTitle = document.createElement('div');
 		divSubTitle.classList.add('sub-title');
-
-		let divTitlePrice = document.createElement('div');
 		divTitlePrice.classList.add('title-price', 'title-item');
-
-		let imgPrice = document.createElement('img');
 		imgPrice.src = '../img/Group 7.png';
-
-		let spanPrice = document.createElement('span');
 		spanPrice.textContent = item.price;
-
-		let divTitleRating = document.createElement('div');
 		divTitleRating.classList.add('title-rating', 'title-item');
-
-		let imgRating = document.createElement('img');
 		imgRating.src = '../img/Group 10.svg';
-
-		let spanRating = document.createElement('span');
 		spanRating.textContent = item.rating.rate;
-
-		let divTitleCount = document.createElement('div');
 		divTitleCount.classList.add('title-count', 'title-item');
-
-		let imgCount = document.createElement('img');
 		imgCount.src = '../img/Group 9.png';
-
-		let spanCount = document.createElement('span');
 		spanCount.textContent = item.rating.count;
-
-		let buttonFavorite = document.createElement('button');
 		buttonFavorite.textContent = 'В избранное';
+
+		
 
 		container_box.append(divBoxItem);
 		divBoxItem.append(divItemTop);
